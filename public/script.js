1a8d2c3a71155e22365b66d351adb17dda86fcb6
Ayushman <ayushmannath270@gmail.com>
2025-05-21 13:16:19 +0530

    Initial commit with all project files including public folder

---
+++ public/script.js
@@ -0,0 +1,6 @@
+// Example: Show alert when ordering
+document.querySelectorAll('.btn-primary').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert("Thanks for your interest! We'll contact you soon.");
+  });
+});
